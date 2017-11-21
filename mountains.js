0aef22c4591aba8363a4abf5921dc417431706db
Ted Cooper <cooperte@onid.oregonstate.edu>
2017-11-20 17:22:06 -0700

    first commit

---
+++ mountains.js
@@ -0,0 +1,12 @@
+var SERVICES = [
+  {Service: "Carpet Cleaning", Cost: 120},
+  {Service: "Tile Cleaning", Cost: 150},
+  {Service: "Ulphostery Cleaning", Cost: 100},
+  {Service: "Strip and Wax", Cost: 200},
+  {Service: "Final Clean", Cost: 500},
+  {Service: "Window Cleaning", Cost: 200},
+  {Service: "Construction Cleanup", Cost: 200}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = SERVICES;
